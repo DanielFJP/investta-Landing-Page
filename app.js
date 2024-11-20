a23490fcf57e128324098d78ab47c5ea57f5ed1a
Daniel Jimenez <danieljimenezpalomino2001@gmail.com>
2024-11-20 16:02:21 -0500

    asignación de flavicon

--- app.js
+++ app.js
@@ -14,30 +14,27 @@ function generateConfetti() {
         const confetti = document.createElement("div");
         confetti.classList.add("confetti");
 
-        // Generar color aleatorio
+       
         confetti.style.backgroundColor =
             colors[Math.floor(Math.random() * colors.length)];
 
-        // Posición inicial
+       
         confetti.style.left = `${Math.random() * 100}%`;
         confetti.style.top = `${Math.random() * 50}px`;
 
-        // Tamaño aleatorio
-        const size = Math.random() * 8 + 4; // Tamaños entre 4px y 12px
+    
+        const size = Math.random() * 8 + 4; 
         confetti.style.width = `${size}px`;
         confetti.style.height = `${size * 2}px`;
 
-        // Animación adicional (para variar)
         confetti.style.animationDelay = `${Math.random() * 2}s`;
 
         confettiContainer.appendChild(confetti);
     }
 
-    // Limpia los confetis después de un tiempo
     setTimeout(() => {
         confettiContainer.innerHTML = "";
     }, 3000);
 }
 
-// Llamar la función cuando sea necesario
 generateConfetti();
